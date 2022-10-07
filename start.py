2e98b6c35a95289c8b9c5916e6ebd6ad234c7b5c
olega211 <106370448+olega211@users.noreply.github.com>
2022-10-07 15:54:05 +0700

    f

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from admin import app
 
-if __name__ == '__main__':
+with app.app_context():
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['JSON_AS_ASCII'] = False
     app.run(debug=True, port=5005)
